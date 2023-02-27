ea7187e55de925bcd6757c0f2b927e4c4412ace8
moomoomoo123 <106563335+moomoomoo123@users.noreply.github.com>
2023-02-26 17:39:34 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 425);
+    canvas.position(710, 165);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("PoseNet is initialized!!");
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("leftWristX = " + leftWristX + " rightWristX = "+ rightWristX + " difference = " + difference);
+    }
+}
+
+function draw() {
+    background('#DCB0FF');
+
+    fill(149, 52, 249);
+    text('arya', 50, 200);
+
+    textSize(difference);
+}
+
+
+
+
+
+
